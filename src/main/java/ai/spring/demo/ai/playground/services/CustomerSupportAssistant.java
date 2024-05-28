f523e7a66a95ea172509316bf32039a0c87cf40c
Christian Tzolov <ctzolov@vmware.com>
2024-05-28 20:10:06 +0200

    Update to 1.0.0-M1

--- src/main/java/ai/spring/demo/ai/playground/services/CustomerSupportAssistant.java
+++ src/main/java/ai/spring/demo/ai/playground/services/CustomerSupportAssistant.java
@@ -19,7 +19,6 @@ package ai.spring.demo.ai.playground.services;
 import reactor.core.publisher.Flux;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
@@ -45,19 +44,16 @@ public class CustomerSupportAssistant {
 				.defaultSystem("""
 						You are a customer chat support agent of an airline named "Funnair".", Respond in a friendly,
 						helpful, and joyful manner.
-
 						Before providing information about a booking or cancelling a booking, you MUST always
 						get the following information from the user: booking number, customer first name and last name.
-
 						Before changing a booking you MUST ensure it is permitted by the terms.
-
 						If there is a charge for the change, you MUST ask the user to consent before proceeding.
 						""")
 				.defaultAdvisors(
 						new PromptChatMemoryAdvisor(chatMemory),
 						// new MessageChatMemoryAdvisor(chatMemory), // CHAT MEMORY
-						new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()),
-						new LoggingAdvisor()) // RAG
+						new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()), // RAG
+						new LoggingAdvisor())
 
 						// new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()
 						// 	.withFilterExpression("'documentType' == 'terms-of-service' && region in ['EU', 'US']")),
