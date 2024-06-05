237021c112f491ec3f999c89c6faf9de0b36d39c
Christian Tzolov <ctzolov@vmware.com>
2024-06-05 15:23:02 +0200

    Minor sys prompt improvements

--- src/main/java/ai/spring/demo/ai/playground/services/CustomerSupportAssistant.java
+++ src/main/java/ai/spring/demo/ai/playground/services/CustomerSupportAssistant.java
@@ -16,6 +16,8 @@
 
 package ai.spring.demo.ai.playground.services;
 
+import java.time.LocalDate;
+
 import reactor.core.publisher.Flux;
 
 import org.springframework.ai.chat.client.ChatClient;
@@ -42,13 +44,18 @@ public class CustomerSupportAssistant {
 		// @formatter:off
 		this.chatClient = modelBuilder
 				.defaultSystem("""
-						You are a customer chat support agent of an airline named "Funnair".", Respond in a friendly,
-						helpful, and joyful manner.
-						Before providing information about a booking or cancelling a booking, you MUST always
-						get the following information from the user: booking number, customer first name and last name.
-						Before changing a booking you MUST ensure it is permitted by the terms.
-						If there is a charge for the change, you MUST ask the user to consent before proceeding.
-						""")
+					You are a customer chat support agent of an airline named "Funnair"."
+					Respond in a friendly, helpful, and joyful manner.
+					You are interacting with customers through an online chat system.
+					Before providing information about a booking or cancelling a booking, you MUST always
+					get the following information from the user: booking number, customer first name and last name.
+					Check the message history for this information before asking the user.
+					Before changing a booking you MUST ensure it is permitted by the terms.
+					If there is a charge for the change, you MUST ask the user to consent before proceeding.
+					Use the provided functions to fetch booking details, change bookings, and cancel bookings.
+					Use parallel function calling if required.
+					Today is {current_date}.
+					""")
 				.defaultAdvisors(
 						new PromptChatMemoryAdvisor(chatMemory),
 						// new MessageChatMemoryAdvisor(chatMemory), // CHAT MEMORY
@@ -70,6 +77,7 @@ public class CustomerSupportAssistant {
 	public Flux<String> chat(String chatId, String userMessageContent) {
 
 		return this.chatClient.prompt()
+				.system(s -> s.param("current_date", LocalDate.now().toString()))
 				.user(userMessageContent)
 				.advisors(a -> a
 						.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
