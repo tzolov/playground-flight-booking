7eb3888eea4554fbc29f855c443a787ffec11e4a
Christian Tzolov <ctzolov@vmware.com>
2024-06-11 10:36:34 +0200

    minor improvements

--- src/main/java/ai/spring/demo/ai/playground/Application.java
+++ src/main/java/ai/spring/demo/ai/playground/Application.java
@@ -1,12 +1,7 @@
 package ai.spring.demo.ai.playground;
 
-import java.io.IOException;
-
-import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.embedding.EmbeddingModel;
@@ -14,11 +9,15 @@ import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.Resource;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 
 @SpringBootApplication
 @Theme(value = "customer-support-agent")
@@ -30,19 +29,18 @@ public class Application implements AppShellConfigurator {
 		new SpringApplicationBuilder(Application.class).run(args);
 	}
 
-	// In the real world, ingesting documents would often happen separately, on a CI server or similar
+	// In the real world, ingesting documents would often happen separately, on a CI
+	// server or similar.
 	@Bean
 	CommandLineRunner ingestTermOfServiceToVectorStore(
 			EmbeddingModel embeddingModel, VectorStore vectorStore,
-			ResourceLoader resourceLoader) throws IOException {
-
-		var termsOfServiceDocs = resourceLoader.getResource("classpath:rag/terms-of-service.txt");
+			@Value("classpath:rag/terms-of-service.txt") Resource termsOfServiceDocs) {
 
 		return args -> {
 			// Ingest the document into the vector store
 			vectorStore.write(
-				new TokenTextSplitter().transform(
-					new TextReader(termsOfServiceDocs).read()));
+					new TokenTextSplitter().transform(
+							new TextReader(termsOfServiceDocs).read()));
 
 			vectorStore.similaritySearch("Cancelling Bookings").forEach(doc -> {
 				logger.info("Similar Document: {}", doc.getContent());
