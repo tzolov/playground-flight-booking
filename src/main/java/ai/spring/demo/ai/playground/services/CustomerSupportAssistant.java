70de72dfb6925e62b13f160fbf71c0a42ba5f30c
Christian Tzolov <ctzolov@vmware.com>
2024-06-11 07:13:19 +0200

    Fix npm issues

--- src/main/java/ai/spring/demo/ai/playground/services/CustomerSupportAssistant.java
+++ src/main/java/ai/spring/demo/ai/playground/services/CustomerSupportAssistant.java
@@ -44,31 +44,29 @@ public class CustomerSupportAssistant {
 		// @formatter:off
 		this.chatClient = modelBuilder
 				.defaultSystem("""
-					You are a customer chat support agent of an airline named "Funnair"."
-					Respond in a friendly, helpful, and joyful manner.
-					You are interacting with customers through an online chat system.
-					Before providing information about a booking or cancelling a booking, you MUST always
-					get the following information from the user: booking number, customer first name and last name.
-					Check the message history for this information before asking the user.
-					Before changing a booking you MUST ensure it is permitted by the terms.
-					If there is a charge for the change, you MUST ask the user to consent before proceeding.
-					Use the provided functions to fetch booking details, change bookings, and cancel bookings.
-					Use parallel function calling if required.
-					Today is {current_date}.
+						You are a customer chat support agent of an airline named "Funnair"."
+						Respond in a friendly, helpful, and joyful manner.
+						You are interacting with customers through an online chat system.
+						Before providing information about a booking or cancelling a booking, you MUST always
+						get the following information from the user: booking number, customer first name and last name.
+						Check the message history for this information before asking the user.
+						Before changing a booking you MUST ensure it is permitted by the terms.
+						If there is a charge for the change, you MUST ask the user to consent before proceeding.
+						Use the provided functions to fetch booking details, change bookings, and cancel bookings.
+						Use parallel function calling if required.
+						Today is {current_date}.
 					""")
 				.defaultAdvisors(
-						// new PromptChatMemoryAdvisor(chatMemory),
+						new PromptChatMemoryAdvisor(chatMemory), // Chat Memory
+						// new VectorStoreChatMemoryAdvisor(vectorStore)),
 					
-						// new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()), // RAG
-						new LoggingAdvisor())
-
+						new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()), // RAG
 						// new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()
 						// 	.withFilterExpression("'documentType' == 'terms-of-service' && region in ['EU', 'US']")),
-
-						// new VectorStoreChatMemoryAdvisor(vectorStore))
-
-
-				// .defaultFunctions("getBookingDetails", "changeBooking", "cancelBooking") // FUNCTION CALLING
+						
+						new LoggingAdvisor())
+						
+				.defaultFunctions("getBookingDetails", "changeBooking", "cancelBooking") // FUNCTION CALLING
 
 				.build();
 		// @formatter:on
