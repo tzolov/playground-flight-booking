192a25323738c92f4b981f063e34216a18375ef5
Christian Tzolov <ctzolov@vmware.com>
2024-09-05 12:16:11 +0200

    Add observability support

--- src/main/java/ai/spring/demo/ai/playground/Application.java
+++ src/main/java/ai/spring/demo/ai/playground/Application.java
@@ -7,7 +7,6 @@ import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -15,9 +14,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+
+import io.micrometer.observation.ObservationPredicate;
 
 @SpringBootApplication
 @Theme(value = "customer-support-agent")
@@ -32,15 +34,12 @@ public class Application implements AppShellConfigurator {
 	// In the real world, ingesting documents would often happen separately, on a CI
 	// server or similar.
 	@Bean
-	CommandLineRunner ingestTermOfServiceToVectorStore(
-			EmbeddingModel embeddingModel, VectorStore vectorStore,
+	CommandLineRunner ingestTermOfServiceToVectorStore(EmbeddingModel embeddingModel, VectorStore vectorStore,
 			@Value("classpath:rag/terms-of-service.txt") Resource termsOfServiceDocs) {
 
 		return args -> {
 			// Ingest the document into the vector store
-			vectorStore.write(
-					new TokenTextSplitter().transform(
-							new TextReader(termsOfServiceDocs).read()));
+			vectorStore.write(new TokenTextSplitter().transform(new TextReader(termsOfServiceDocs).read()));
 
 			vectorStore.similaritySearch("Cancelling Bookings").forEach(doc -> {
 				logger.info("Similar Document: {}", doc.getContent());
@@ -48,13 +47,32 @@ public class Application implements AppShellConfigurator {
 		};
 	}
 
-	@Bean
-	public VectorStore vectorStore(EmbeddingModel embeddingModel) {
-		return new SimpleVectorStore(embeddingModel);
-	}
+	// @Bean
+	// @ConditionalOnMissingBean
+	// public VectorStore vectorStore(EmbeddingModel embeddingModel) {
+	// return new SimpleVectorStore(embeddingModel);
+	// }
 
 	@Bean
 	public ChatMemory chatMemory() {
 		return new InMemoryChatMemory();
 	}
+
+	// Optional suppress the actuator server observations. This hides the actuator
+	// prometheus traces.
+	@Bean
+	ObservationPredicate noActuatorServerObservations() {
+		return (name, context) -> {
+			if (name.equals("http.server.requests")
+					&& context instanceof ServerRequestObservationContext serverContext) {
+				String requestUri = serverContext.getCarrier().getRequestURI();
+				return !requestUri.startsWith("/actuator") && !requestUri.startsWith("/VAADIN")
+						&& !requestUri.startsWith("/HILLA") && !requestUri.startsWith("/connect");
+			}
+			else {
+				return true;
+			}
+		};
+	}
+
 }
